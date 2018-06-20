3ec0d5f6203fc9cb400dc1ea6f55fe31f998303f
Asawavetvutt Varit <as.varit_st@tni.ac.th>
2018-06-20 21:55:51 +0900

    cancel action of form only in case that user input the zenkaku

--- char_tran.js
+++ char_tran.js
@@ -4,19 +4,25 @@ var str;
 $('form#form').submit(function (event) {
 
   //this will cancel action of form
-  event.preventDefault();
+  //event.preventDefault();
 
   var $form = $(this),//this is $('form#form')
     str = $form.find( 'input#text' ).val(),
-    all_str = "";//get value
+    all_str = "",
+    str_pattern = /[Ａ-Ｚａ-ｚ０-９]/g;//get value
 
-  str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
-      console.log("before: ", s);
-      str = String.fromCharCode(s.charCodeAt(0) - 65248);
-      all_str += str;
-      return str;
-  });
-  console.log(all_str);
+  if( str.match(str_pattern) ){
+    alert("please input in hankaku");
+    event.preventDefault();
+    str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+        console.log("before: ", s);
+        str = String.fromCharCode(s.charCodeAt(0) - 65248);
+        all_str += str;
+        return str;
+    });
 
-  $form.find( 'input#text' ).val(all_str);
+    console.log(all_str);
+    $form.find( 'input#text' ).val(all_str);
+  }
+
 });
